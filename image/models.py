8db0370d2d24e7aa7f5b1c4b3c61468f7f395a1c
SandipNup <sandipneupane65@gmail.com>
2020-01-16 12:03:29 +0545

    signal to create deleted record of models

--- image/models.py
+++ image/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-import base64
-from PIL import Image
-import os
-from django.conf import settings
-
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -15,38 +12,21 @@ class ImageData(models.Model):
     def __str__(self):
         return self.name
 
-    # def image_as_base64(self):
-    #     # print(str(self.image))
-    #     with open('media/' + str(self.image), "rb") as img_file:
-    #         img_encode = base64.b64encode(img_file.read()).decode("utf-8")
-    #         print(img_encode)
-    #
-    #     # print("aaaaa")
-    #     # print(base64.b64encode(image))
-    #
-    #     # if self.image:
-    #
-    #     # with open(image, "rb") as img_file:
-    #     #     encoded_string = base64.b64encode(img_file.read())
-    #     #     decoded_string = encoded_string.decode("utf-8")
-    #     #
-    #     #     print(decoded_string)
-    #
-    #     # image = Image.open(self.image)
-    #
-    #     # image_name, image_extension = os.path.splitext(self.image.name)
-    #     # image_extension = image_extension.lower()
-    #     #
-    #     # self.encode = 'data:image/%s;base64,%s' % {image_extension,decoded_string}
-    #
-    #     return True
-    #
-    # def save(self, *args, **kwargs):
-    #     if not self.image_as_base64():
-    #         raise Exception('Could not create base64 of image')
-    #     super(ImageData, self).save(*args, **kwargs)
+
 
 
 class DeletedImage(models.Model):
     image_id = models.IntegerField()
+
+
+
+
+#signals
+@receiver(post_delete, sender=ImageData)
+def my_handler(sender, **kwargs):
+    instance = kwargs['instance']
+    image_id = instance.id
+    delted_image = DeletedImage.objects.create(image_id=image_id)
+
+
 
