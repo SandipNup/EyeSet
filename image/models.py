2b5bbc5664b7d46736d0d66b5d744fa3fd525f8b
sumit <rounnn8@gmail.com>
2020-07-08 11:26:43 +0545

    new test option added

--- image/models.py
+++ image/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
+
 # Create your models here.
 
 
 class ImageData(models.Model):
     WARD_CHOICES = (
         ('0', 'All Ward'),
+        ('test', 'test'),
         ('lmc2', 'LMC 2'),
         ('lmc9', 'LMC 9'),
         ('lmc11', 'LMC 11'),
@@ -20,25 +22,17 @@ class ImageData(models.Model):
     image = models.FileField()
     ward = models.CharField(choices=WARD_CHOICES, default='0', max_length=15, blank=True, null=True)
 
-
     def __str__(self):
         return self.name
-
-
 
 
 class DeletedImage(models.Model):
     image_id = models.IntegerField()
 
 
-
-
-#signals
+# signals
 @receiver(post_delete, sender=ImageData)
 def my_handler(sender, **kwargs):
     instance = kwargs['instance']
     image_id = instance.id
     delted_image = DeletedImage.objects.create(image_id=image_id)
-
-
-
